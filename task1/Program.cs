25d482e28e57226b7830e86465b10c44f17a9f71
Anna <ds_anna@bk.ru>
2022-06-20 16:03:41 +0300

    Показать натуральные числа от M до N

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void PrintNumbers(int min, int max)
+{
+    if (max < min)
+    {
+        Console.WriteLine();
+        return;
+    }
+    Console.Write(min + " ");
+    PrintNumbers(min + 1, max);
+}
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+if (m <= 0 || n <= 0)
+    Console.WriteLine("Введены не натуральные числа!");
+else if (m > n)
+{
+    int tmp = m; m = n; n = tmp;
+    PrintNumbers(m, n);
+}
+else PrintNumbers(m, n);
